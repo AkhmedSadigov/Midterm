296be4ef90f371e8b08c90ec06383a1835794926
ahmed <ahmed@LAPTOP-D0D6I3G9>
2022-10-31 12:39:43 +0400

    Add project files.

---
+++ Midterm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double p = 3.14;
+                double r;
+
+                Console.Write("Çevrenin sahesi: ");
+                int sahe = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Çevrenin düsturu: p(r*r)");
+
+                r = Math.Sqrt(sahe / p);
+
+                Console.WriteLine("Çevrenin radiusu: " + r);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("\nZehmet olmasa duzgun ifade daxil edin!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
